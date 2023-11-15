4a2dc55224299b37366001d1085f4d1227fc3de7
AndoniUTT <andoni2020lopez@gmail.com>
2023-11-15 15:05:58 -0600

    02 Creación de variables y metodos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
+
+    public static int ternarySearch(int[] array, int i, int k, int key){
+    }
+
+
     public static void main(String[] args) throws Exception {
-        
+        int[] testArray = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int keyToSearch = 23; //Usaré una key predifinida para pruebas, despues se modificará para que el usario la digite
+
+        int result = ternarySearch(testArray, 0, testArray.length - 1, keyToSearch);
     }
 }
